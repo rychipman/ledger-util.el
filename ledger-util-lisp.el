478b9cb592ad6682fd0a38d2123f0c229ac6db67
Ryan Chipman <ryan@ryanchipman.com>
2018-07-06 17:14:01 -0400

    Get some data-driven transaction matching working

---
+++ ledger-util-lisp.el
@@ -0,0 +1,100 @@
+;;; ledger-util-lisp.el --- Helper code for use with ledger-mode.el -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;; This file provides helpers for working with the sexps output by ledger's "lisp" command.
+
+;;; Code:
+
+(require 'seq)
+
+(defvar ledger-util-active-xacts
+  "List of xacts in 'ledger-util-active-file.")
+
+(defvar ledger-util-staged-xacts
+  "List of xacts in 'ledger-util-staging-file.")
+
+(defun ledger-util-load ()
+  "Read ledger xacts as sexps and save in variables."
+  (interactive)
+  (setq ledger-util-active-xacts (read (shell-command-to-string (format "ledger lisp -f %s" ledger-util-active-file)))
+        ledger-util-staged-xacts (read (shell-command-to-string (format "ledger lisp -f %s" ledger-util-staging-file)))))
+
+(defun ledger-util-xact-file (xact)
+  "Get the filename from XACT."
+  (car xact))
+
+(defun ledger-util-xact-line (xact)
+  "Get the line number from XACT."
+  (cadr xact))
+
+(defun ledger-util-xact-date (xact)
+  "Get the date string from XACT."
+  (format-time-string "%Y/%m/%d" (nth 3 xact)))
+
+(defun ledger-util-xact-payee (xact)
+  "Get the payee from XACT."
+  (nth 4 xact))
+
+(defun ledger-util-xact-postings (xact)
+  "Get the postings from XACT."
+  (nthcdr 5 xact))
+
+(defun ledger-util-xact-amts (xact)
+  "Get the amounts from all the postings in XACT."
+  (mapcar 'ledger-util-post-amt (ledger-util-xact-postings xact)))
+
+(defun ledger-util-xact-cleared (xact)
+  "Check whether all postings in XACT are cleared."
+  (seq-every-p
+   'ledger-util-post-cleared
+   (ledger-util-xact-postings xact)))
+
+(defun ledger-util-xact-uncleared (xact)
+  "Check whether XACT has one or more uncleared postings."
+  (not (ledger-util-xact-cleared xact)))
+
+(defun ledger-util-xact-filter-postings (pred xact)
+  "Filter the postings in XACT according to PRED."
+  (seq-filter
+   pred
+   (ledger-util-xact-postings xact)))
+
+(defun ledger-util-xact-map-postings (func xact)
+  "Map FUNC over the postings in XACT."
+  (seq-map
+   func
+   (ledger-util-xact-postings xact)))
+
+(defun ledger-util-xact-has-amt (amt xact)
+  "Check whether XACT contains a posting with an amount equal to AMT."
+  (not (= 0
+		 (seq-count
+		  '(lambda (post) (string-equal amt (ledger-util-post-amt post)))
+		 (ledger-util-xact-postings xact)))))
+
+(defun ledger-util-post-line (post)
+  "Get the line number from POST."
+  (car post))
+
+(defun ledger-util-post-acct (post)
+  "Get the account from POST."
+  (nth 1 post))
+
+(defun ledger-util-post-amt (post)
+  "Get the amount from POST."
+  (nth 2 post))
+
+(defun ledger-util-post-cleared (post)
+  "Get the cleared status of POST."
+  (nth 3 post))
+
+(defun ledger-util-post-ofxid-tag (post)
+  (nth 4 post))
+
+(defun ledger-util-post-has-ofxid-tag (post)
+  (ledger-util-post-ofxid-tag post)
+  (nth 4 post))
+
+(provide 'ledger-util-lisp)
+
+;;; ledger-util-lisp.el ends here
---
+++ ledger-util.el
@@ -0,0 +1,92 @@
+;;; ledger-util.el --- Helper code for use with ledger-mode.el -*- lexical-binding: t; -*-
+
+;;; Commentary:
+;; Most of the general ledger-util code is here.
+
+;;; Code:
+
+(require 'seq)
+(require 'ledger-util-lisp)
+
+(defvar ledger-util-active-file
+  "Active ledger file. ledger-util assumes that a single file is used.")
+
+(defvar ledger-util-staging-file
+  "File where externally-sourced transactions are stored while awaiting manual import/matching.")
+
+(defun ledger-util-postings-in-xacts (xacts)
+  "Get all the postings from the xacts in XACTS."
+  (apply #'append (seq-map #'ledger-util-xact-postings xacts)))
+
+(defun ledger-util-post-awaiting-match (post)
+  "Return whether posting POST is a valid matching target."
+  (not
+   (or
+    (ledger-util-post-cleared post)
+	(string-prefix-p "Budget" (ledger-util-post-acct post))
+	(string-prefix-p "Equity" (ledger-util-post-acct post))
+	(string-prefix-p "Expenses" (ledger-util-post-acct post))
+	(string-match-p (regexp-quote ":matched:") (or (ledger-util-post-ofxid-tag post) ""))
+	(string-match-p (regexp-quote "ofxid:") (or (ledger-util-post-ofxid-tag post) ""))
+	)))
+
+(defun ledger-util-post-for-match (post)
+  "Return whether POST is a valid posting for matching."
+  (ledger-util-post-has-ofxid-tag post))
+
+(defun ledger-util-get-postings-awaiting-match ()
+  "Get all postings in 'ledger-util-active-file that are valid match targets."
+  (let (posts)
+	(setq posts (ledger-util-postings-in-xacts ledger-util-active-xacts))
+	(seq-filter #'ledger-util-post-awaiting-match posts)))
+
+(defun ledger-util-get-postings-for-match ()
+  "Get all match-eligible postings from 'ledger-util-staging-file."
+  (let (posts)
+	(setq posts (ledger-util-postings-in-xacts ledger-util-staged-xacts))
+	(seq-filter #'ledger-util-post-for-match posts)))
+
+(defun ledger-util-postings-by-amount (posts)
+  "Group the postings in POSTS into an alist by their amount."
+  (seq-group-by #'ledger-util-post-amt posts))
+
+(defun ledger-util-get-posts-for-match-with-candidates ()
+  "Get an alist of match-eligible postings to candidate match targets based on posting amount."
+  (let (awaiting-by-amount)
+	(setq awaiting-by-amount (ledger-util-postings-by-amount (ledger-util-get-postings-awaiting-match)))
+	(seq-map
+	 (lambda (post)
+	   (list post (cdr (assoc (ledger-util-post-amt post) awaiting-by-amount))))
+	 (ledger-util-get-postings-for-match))))
+
+(defun ledger-util-get-matchable-posts ()
+  "Filter the output of 'ledger-util-get-posts-for-match-with-candidates to exclude postings with no candidate targets."
+  (let (posts-with-candidates matchable)
+	(setq posts-with-candidates (ledger-util-get-posts-for-match-with-candidates))
+	(seq-filter (lambda (x) (cadr x)) posts-with-candidates)))
+
+(defun ledger-util-match-first (&optional n)
+  (interactive)
+  (setq n (or n 0))
+  (let (matchable first-matchable post candidates candidate)
+	(setq matchable (ledger-util-get-matchable-posts))
+	(setq first-matchable (car matchable))
+	(setq post (car first-matchable))
+	(setq candidates (cadr first-matchable))
+	(setq candidate (nth n candidates))
+
+	(delete-other-windows)
+
+	(find-file ledger-util-staging-file)
+	(goto-char (point-min))
+	(forward-line (1- (ledger-util-post-line post)))
+
+	(find-file-other-window ledger-util-active-file)
+	(goto-char (point-min))
+	(forward-line (1- (ledger-util-post-line candidate)))
+	)
+  )
+
+(provide 'ledger-util)
+
+;;; ledger-util.el ends here
