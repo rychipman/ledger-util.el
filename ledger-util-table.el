6adb5d82394f17eee28d2f95231ef4fcf5369730
Ryan Chipman <ryan@ryanchipman.com>
2018-07-23 18:15:17 -0400

    Get scaffold for interactive match selection working

--- ledger-util-table.el
+++ ledger-util-table.el
@@ -44,13 +44,10 @@
   (unless (> (length cols) 0)
 	(error "Must provide at least one column name"))
   (setq tabulated-list-format (cl-map 'vector #'lu-table-col-from-name cols))
-  (setq tabulated-list-entries (lambda () (lu-table-get-entries-from-names cols)))
-  (tabulated-list-init-header)
-  (tablist-revert))
+  (setq tabulated-list-entries (lambda () (lu-table-get-entries-from-names cols))))
 
 (defun lu-table-use-filters (&rest filters)
-  (setq tablist-current-filter (lu-table-get-filter-conjunction filters))
-  (tablist-revert))
+  (setq tablist-current-filter (lu-table-get-filter-conjunction filters)))
 
 (defun lu-table-get-filter-conjunction (names)
   (let (conj)
@@ -75,25 +72,81 @@
 	)
   )
 
+(defun lu-table-get-amount-filter (amt)
+  `(== "Amount" ,amt))
+
 (defun lu-table-configure ()
   (lu-table-use-columns "date-str" "amount" "payee" "account" "file" "cleared-str")
-  (lu-table-use-filters "account" "staged"))
+  (lu-table-use-filters "account" "staged")
+  (tabulated-list-init-header)
+  (tablist-revert))
+
+(defvar lu-table-current-view nil
+  "Name of the current view.")
 
 (defun lu-table-set-view (name)
   (pcase name
+	("all"
+	 (lu-table-use-columns "date-str" "amount" "payee" "account" "file" "cleared-str")
+	 (lu-table-use-filters))
 	("import"
 	 (lu-table-use-columns "date-str" "amount" "payee" "account" "file")
 	 (lu-table-use-filters "account" "staged"))
 	("matchable"
 	 (lu-table-use-columns "date-str" "amount" "payee" "account" "file" "cleared-str")
 	 (lu-table-use-filters "account" "main" "uncleared"))
+	(_ (error "Invalid view name %s" name))
 	)
-  )
+  (setq lu-table-current-view name)
+  (tabulated-list-init-header)
+  (tablist-revert))
 
 (define-derived-mode lu-table-mode
   tablist-mode "lu-table"
   "Major mode for displaying ledger data in a table."
+  (setq lu-table-current-view-name nil)
+  (linum-mode -1)
   (lu-table-configure))
+
+(magit-define-popup lu-table-match-help-popup
+  "Help popup for lu-table-match-mode."
+  :actions '("Ledger xact matching help"
+			 (?X "Exec" 'lu-table-match-marked)
+			 (?M "Match" 'lu-table-match-select)
+			 (?m "Mark")))
+
+(defvar lu-table-match-mode-map nil
+  "Keymap for `lu-table-match-mode'.")
+
+(defun lu-table-match-select ()
+  (interactive)
+  (tablist-put-mark)
+  (pcase lu-table-current-view
+	("import" (lu-table-set-view "matchable"))
+	("matchable" (lu-table-match-marked))
+	(_ (error "In unexpected view state %s" lu-table-current-view))
+	)
+  )
+
+(defun lu-table-match-marked ()
+  (interactive)
+  (lu-table-set-view "all")
+  (let (marked)
+	(setq marked (tablist-get-marked-items))
+	(tablist-unmark-all-marks)
+	(message "Marked XACTs:")
+	(dolist (ent marked)
+	  (message "  %s" ent)))
+  (lu-table-set-view "import"))
+
+(setq lu-table-match-mode-map (make-sparse-keymap))
+(define-key lu-table-match-mode-map (kbd "?") 'lu-table-match-help-popup)
+(define-key lu-table-match-mode-map (kbd "M") 'lu-table-match-select)
+
+(define-derived-mode lu-table-match-mode
+  lu-table-mode "lu-table-match"
+  "Major mode for choosing a transaction to match."
+  (lu-table-set-view "import"))
 
 (provide 'ledger-util-table)
 
--- ledger-util.el
+++ ledger-util.el
@@ -20,18 +20,10 @@
   (pop-to-buffer lu-table-buffer-name)
   (lu-table-mode))
 
-;;(setq lu-match-mode-highlights
-;;	  '(("Liabilities\\|Assets" . font-lock-function-name-face)
-;;		("..../../...+\\$-[0-9]+\.[0-9][0-9]" . font-lock-constant-face)
-;;		("..../../...+\\$[0-9]+\.[0-9][0-9]" . font-lock-doc-face)
-;;		))
-;;
-;;(magit-define-popup lu-match-help-popup
-;;  "Help popup for lu-match."
-;;  :actions '("Ledger Matching Help"
-;;			 (?d "Delete")
-;;			 (?a "Add")
-;;			 (?m "Match")))
+(defun lu-match ()
+  (interactive)
+  (pop-to-buffer lu-table-buffer-name)
+  (lu-table-match-mode))
 
 (provide 'ledger-util)
 
